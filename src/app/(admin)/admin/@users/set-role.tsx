340bded4f390965049c8df198cbf9cd45b1fda47
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-07 17:20:34 +0600

    ✨ feat: create set role

---
+++ src/app/(admin)/admin/@users/set-role.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogFooter, DialogHeader, DialogContent, DialogTitle } from '@/components/ui/dialog';
+import { useTransition } from 'react'
+import { setRole } from './actions';
+import { Roles } from '@/global';
+
+export default function SetRole({ userId, role, open, setOpen }: { userId: string, role: Roles, open: boolean, setOpen: (value: boolean) => void }) {
+    const [isPending, startTransition] = useTransition();
+
+    return (
+        <>
+            <Dialog open={open} onOpenChange={setOpen}>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Are you sure to make him moderator?</DialogTitle>
+                    </DialogHeader>
+                    <DialogFooter>
+                        <Button disabled={isPending} variant={'ghost'} onClick={() => setOpen(false)}>No</Button>
+                        <Button disabled={isPending} onClick={
+                            async () => {
+                                startTransition(async () => {
+                                    await setRole(userId, role)
+                                })
+                            }
+                        } variant={'destructive'}>
+                            {isPending ? 'Making moderator...' : 'Yes, confirm'}
+                        </Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </>
+    )
+}
