949ef9f9cf0ac012b66a7b6bcdbd95b89e65614e
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-07 14:30:55 +0600

    ✨ feat: dynamic btn in nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import { Button } from './ui/button'
 import { ModeToggle } from './mode-toggle'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
 
 export default function Navbar() {
     return (
         <nav className='p-4 fixed backdrop-blur-2xl top-0 w-full h-[80px] border-b flex items-center justify-between'>
-            <div>Logo</div>
+            <Link className='font-bold cursor-pointer' href="/">Logo</Link>
 
             <div className='flex items-center gap-2'>
                 <ModeToggle />
-                <Button>Sign up</Button>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+                <SignedOut>
+                    <Link href='/sign-in'>
+                        <Button>Sign In</Button>
+                    </Link>
+                </SignedOut>
             </div>
         </nav>
     )
