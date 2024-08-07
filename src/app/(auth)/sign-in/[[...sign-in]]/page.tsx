52b9e34857937a17ef5194a8670530c0cddfffb5
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-07 14:46:33 +0600

    ✨ feat: add meta tag

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignIn } from "@clerk/nextjs";
 
+export const metadata = {
+    title: "Sign in",
+    description: "Sign in to your account",
+};
+
 export default function Page() {
     return <div className="grid place-items-center">
         <SignIn />
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignUp } from "@clerk/nextjs";
 
+export const metadata = {
+    title: "Sign up",
+    description: "Sign up to your account",
+};
+
 export default function Page() {
     return <div className="grid place-items-center">
         <SignUp />
