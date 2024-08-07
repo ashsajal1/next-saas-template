822925e3a6f20130b578787411bd9b9600ffdd43
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-07 15:41:42 +0600

    ✨ feat: add home link btn

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,23 @@
-import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import { Card, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import Link from 'next/link'
 import React from 'react'
 
 export default function NotFound() {
-  return (
-    <div>
-        <Card>
-            <CardHeader>
-                <CardTitle>404 - Not Found</CardTitle>
-            </CardHeader>
-        </Card>
-    </div>
-  )
+    return (
+        <div>
+            <Card>
+                <CardHeader>
+                    <CardTitle>404 - Not Found</CardTitle>
+                </CardHeader>
+                <CardFooter>
+                    <Link href="/">
+                        <Button>
+                            Home
+                        </Button>
+                    </Link>
+                </CardFooter>
+            </Card>
+        </div>
+    )
 }
