c0ce933061f7a2474273205d7c2a81b65fc2de57
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-07 15:40:33 +0600

    ✨ feat: add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div>
+        <Card>
+            <CardHeader>
+                <CardTitle>404 - Not Found</CardTitle>
+            </CardHeader>
+        </Card>
+    </div>
+  )
+}
