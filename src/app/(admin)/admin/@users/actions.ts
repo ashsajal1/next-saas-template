53d12929c5d5eff689cb3bd73880c5c4c0ec303f
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-07 17:23:29 +0600

    ✨ feat: add action

---
+++ src/app/(admin)/admin/@users/actions.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { Roles } from "@/global";
+import { checkRole } from "@/lib/roles";
+import { clerkClient } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+export async function setRole(userId: string, role: Roles) {
+  // Check that the user trying to set the role is an admin
+  if (!checkRole("admin")) {
+    return { message: "Not Authorized" };
+  }
+
+  try {
+    const isMod = (await clerkClient.users.getUser(userId)).publicMetadata.role;
+    if(role === 'moderator') {
+      await clerkClient.users.updateUser(userId, {
+        publicMetadata: { role: 'user' },
+      });
+      revalidatePath("/");
+      return;
+    }
+
+    const res = await clerkClient.users.updateUser(userId, {
+      publicMetadata: { role: role },
+    });
+    revalidatePath("/");
+    return { message: res.publicMetadata };
+  } catch (err) {
+    return { message: err };
+  }
+}
