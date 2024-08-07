44cce421e3568e79fd266e88443c25c4e75720e4
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-07 15:55:49 +0600

    ✨ feat: add about page

---
+++ src/app/(common)/about/page.tsx
@@ -0,0 +1,26 @@
+export default function About() {
+  return (
+    <div>
+        <h2 className="text-2xl font-semibold mb-4">About</h2>
+        <p className="mb-4">
+          This website is a collection of small and simple tools that I find
+          useful. It&apos;s a place where I can share my ideas, prototypes, and
+          experiments.
+        </p>
+        <p className="mb-4">
+          The tools on this site are built using modern web technologies and
+          designed to be easy to use and understand. Each tool is documented
+          with examples and explanations to help you get started.
+        </p>
+        <p className="mb-4">
+          I hope you find something useful here. If you have any questions or
+          feedback, please feel free to reach out.
+        </p>
+        <p className="">
+          <span className="font-semibold">Note:</span> This site is in the
+          early stages of development. Over time, I will be adding more tools and
+          improving the overall experience.
+        </p>
+    </div>
+  )
+}
