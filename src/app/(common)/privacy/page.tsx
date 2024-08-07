bc433eec2ab608bd6e0c99dc06520b61fba7a5b4
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-07 16:03:15 +0600

    ✨ feat: add privacy page

---
+++ src/app/(common)/privacy/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Privacy() {
+    return (
+        <div>
+            <h2 className="text-2xl font-semibold mb-4">Privacy</h2>
+            <p className="mb-4">
+                This website is a collection of small and simple tools that I find
+                useful. It&apos;s a place where I can share my ideas, prototypes, and
+                experiments.
+            </p>
+            <p className="mb-4">
+                The tools on this site are built using modern web technologies and
+                designed to be easy to use and understand. Each tool is documented
+                with examples and explanations to help you get started.
+            </p>
+        </div>
+    )
+}
