add032186eb88272fad70b3dc067e59c075a41e1
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-08 23:07:00 +0600

    ✨ feat: add dynamic cache

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = "my-cache";
+const STATIC_CACHE = "web-static-cache";
+const DYNAMIC_CACHE = "web-dynamic-cache";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(["/"]);
+    caches.open(STATIC_CACHE).then((cache) => {
+      return cache.addAll(["/", "/about"]);
     })
   );
 });
@@ -11,7 +12,16 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      if (response) {
+        return response; // Return the cached response if it exists
+      }
+
+      return fetch(event.request).then((networkResponse) => {
+        return caches.open(DYNAMIC_CACHE).then((cache) => {
+          cache.put(event.request, networkResponse.clone()); // Cache the fetched response
+          return networkResponse;
+        });
+      });
     })
   );
 });
