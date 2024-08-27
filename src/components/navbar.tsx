36fc74d59d3cf072b1149374f4552d1c98b00c32
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-27 08:28:32 +0600

    🦄 refactor: replace user btn

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Button } from './ui/button'
 import { ModeToggle } from './mode-toggle'
-import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { SignedIn, SignedOut } from '@clerk/nextjs'
 import Link from 'next/link'
+import { currentUser } from '@clerk/nextjs/server'
+import Image from 'next/image'
 
-export default function Navbar() {
+export default async function Navbar() {
+    const user = await currentUser();
     return (
         <nav className='p-4 z-10 fixed backdrop-blur-2xl top-0 w-full h-[80px] border-b flex items-center justify-between'>
             <Link className='font-bold cursor-pointer' href="/">Logo</Link>
@@ -12,7 +15,7 @@ export default function Navbar() {
             <div className='flex items-center gap-2'>
                 <ModeToggle />
                 <SignedIn>
-                    <UserButton />
+                    <Image width={40} height={40} src={user?.imageUrl!} alt={user?.fullName!} className='rounded-full' />
                 </SignedIn>
                 <SignedOut>
                     <Link href='/sign-in'>
