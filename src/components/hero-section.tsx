f8d74b644f5237483f25672a31f3763358dd39c0
Ashsajal <ashsajal1@gmail.com>
2025-03-20 15:36:19 +0600

    ✨ feat: update HeroSection component for improved formatting and add "use client" directive

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import { Button } from 'react-day-picker'
+"use client";
+import { Button } from "react-day-picker";
 
 export default function HeroSection() {
   return (
-    <div className='p-6 grid place-items-center gap-2'>
-      <h1 className='text-2xl md:text-5xl font-bold'>Welcome to our website!</h1>
-      <div className='flex items-center gap-2'>
+    <div className="p-6 grid place-items-center gap-2">
+      <h1 className="text-2xl md:text-5xl font-bold">
+        Welcome to our website!
+      </h1>
+      <div className="flex items-center gap-2">
         <Button>Explore</Button>
         <Button>Explore</Button>
       </div>
     </div>
-  )
+  );
 }
