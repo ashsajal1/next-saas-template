bd99900673222b9e4dda5ae58c78f83e28cf398f
Ashsajal <ashsajal1@gmail.com>
2025-03-20 15:44:37 +0600

    ✨ feat: use only cafc03 color

--- src/components/nprogress.tsx
+++ src/components/nprogress.tsx
@@ -1,16 +1,14 @@
 // Create a Providers component to wrap your application with all the components requiring 'use client', such as next-nprogress-bar or your different contexts...
-'use client';
+"use client";
 
-import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
-import { useTheme } from "next-themes"
+import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 
 const Nprogress = () => {
-  const { theme } = useTheme();
   return (
     <>
       <ProgressBar
         height="3px"
-        color={theme === 'dark'? '#fff':'#000'}
+        color={"#cafc03"}
         options={{ showSpinner: false }}
         shallowRouting
       />
